e1e3b042e12d697d8fa33720be4a32228d53491a
Cindy Leoni <you@example.com>
2022-06-08 16:03:45 +0700

    feat: add, update todo item

--- src/App.js
+++ src/App.js
@@ -3,12 +3,18 @@ import * as React from "react";
 import { ChakraProvider, Input } from "@chakra-ui/react";
 import { TodoItem } from "./todoitem";
 
-function InputForm({setTodo }) {
+function InputForm({ todo, setTodo }) {
   const input = React.useRef("");
   function handleSubmit(event) {
     event.preventDefault();
-    setTodo((currentValue) => [...currentValue, {value: input.current.value, checked: false}]);
-  }
+
+    const exist = todo.findIndex(x => x.value==input.current.value) !== -1
+    if (exist){    
+      alert("Sudah ada")
+    } else {
+      setTodo((currentValue) => [...currentValue, {value: input.current.value, checked: false}]);
+    }
+   }
 
   return (
     <form onSubmit={handleSubmit}>
@@ -25,10 +31,27 @@ function Container() {
     return localItem !== null ? JSON.parse(localItem) : []
   });
 
+  function handleChecked(value) {
+    const temp = [...todo]
+    const index = todo.findIndex(x => x.value === value)
+    temp[index].checked = !temp[index].checked
+    setTodo(temp)
+  }
+
+  const complete = todo.filter((item) => {
+    return item.checked === true
+  })
+
+  const incomplete = todo.filter((item) => {
+    return item.checked === false
+  })
+
   return (
     <div>
       <InputForm todo={todo} setTodo={setTodo} />
-      <TodoItem todo={todo} />
+      <TodoItem todo={incomplete} onChecked={handleChecked} />
+      <br />================================<br/>
+      <TodoItem todo={complete} onChecked={handleChecked} />
     </div>
   );
 }
--- src/todoitem.js
+++ src/todoitem.js
@@ -1,16 +1,16 @@
 import * as React from 'react'
 import {Checkbox, UnorderedList, ListItem} from '@chakra-ui/react';
 
-function TodoItem({todo}){
+function TodoItem({todo, onChecked}){
     function  renderList() {
-      console.log(todo)
-      return todo.map((item) => {
-        return (
-            <ListItem>
-                <Checkbox checked={item.checked}>{item.value}</Checkbox>
-            </ListItem>
+        return todo.map((item) => {
+            console.log(item.value, item.checked)
+            return (
+                <ListItem key={item.value}>
+                    <Checkbox defaultChecked={item.checked} onChange={() => {onChecked(item.value)}}>{item.value}</Checkbox>
+                </ListItem>
             )
-      })
+        })
     }
   
     React.useEffect(() => {  
